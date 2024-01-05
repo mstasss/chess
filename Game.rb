6cb74fd5a8a445233a2baab28bc4ced87f89ade6
Vickhammer <tvickhammer@gmail.com>
2024-01-05 13:44:59 -0700

    I added skeletons of the Game,Players and Display classes.

---
+++ Game.rb
@@ -0,0 +1,37 @@
+require_relative 'board.rb'
+require_relative 'cursor.rb'
+require_relative './players/player.rb'
+
+
+
+class Game 
+
+    def initialize ()
+        @board = Board.new
+        @display = Display.new
+        @players = {player_1: Player.new(:white), player_2: Player.new(:black)}
+        @current_player = :player_1
+    end
+
+    def play
+
+        notify_players
+        HumanPlayer.make_move()
+        swap_turn! 
+
+    end
+
+
+  private
+    
+    def notify_players
+        puts "it's #{@current_player}'s turn"
+    end
+
+    def swap_turn! 
+        @current_player == :player_1 ? @current_player = :player_2 : @current_player = :player_1
+    end
+end
+
+# chess = Game.new
+# chess.play
---
+++ display.rb
@@ -0,0 +1,13 @@
+require_relative 'board.rb'
+require_relative 'cursor.rb'
+
+class Display
+
+    def initialize(board)
+        @board = Board.new
+    end
+
+    def render
+    end
+
+end
