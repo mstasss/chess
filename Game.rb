ff159c69eee8540adf794027a6e6e0e5e8cada0b
Vickhammer <tvickhammer@gmail.com>
2024-01-08 14:54:18 -0700

    fixed unnessesary change

--- Game.rb
+++ Game.rb
@@ -1,6 +1,6 @@
 require_relative 'board.rb'
 require_relative 'cursor.rb'
-require_relative './players/player.rb'
+require_relative './players/humanPlayer.rb'
 
 
 
@@ -9,8 +9,8 @@ class Game
     def initialize ()
         @board = Board.new
         @display = Display.new(@board)
-        @players = {player_1: Player.new(:white), player_2: Player.new(:black)}
-        @current_player = :player_1.color
+        @players = {player_1: Player.new(:white, @display), player_2: Player.new(:black, @display)}
+        @current_player = :player_1
     end
 
     def play
@@ -29,7 +29,7 @@ class Game
     end
 
     def swap_turn! 
-        @current_player.color == :white ? @current_player = :player_2 : @current_player = :player_1
+        @current_player == :player_1 ? @current_player = :player_2 : @current_player = :player_1
     end
 end
 
